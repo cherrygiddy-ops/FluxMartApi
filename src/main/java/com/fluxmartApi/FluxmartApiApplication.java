eb2680a18c292e10a584e47303d86a8767c121ae
Morris <morrischerryg@gmail.com>
2025-10-20 13:05:25 +0300

    Initial Commit

---
+++ src/main/java/com/fluxmartApi/FluxmartApiApplication.java
@@ -0,0 +1,13 @@
+package com.fluxmartApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxmartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxmartApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluxmartApi/FluxmartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxmartApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxmartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
