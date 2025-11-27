58dd09643fac5921eb2e33fc39d2b49a8b6eab1c
Morris <morrischerryg@gmail.com>
2025-11-27 10:49:19 +0300

    database migrations

--- src/main/resources/db/migration/V2__createTable_Cart.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `sql_store`.`cart` (
-  `id` BINARY(16) NOT NULL DEFAULT (uuid_to_bin(uuid())),
-  `createdAt` DATE NOT NULL DEFAULT (curdate()),
-  PRIMARY KEY (`id`));
--- src/main/resources/db/migration/V3__add_Constraints_to_cart_items.sql
+++
@@ -1,9 +0,0 @@
-
-  ALTER TABLE `sql_store`.`cart_items`
- ADD constraint cart_items_cart_product_unique
-      unique (cart_id, product_id),
-  ADD CONSTRAINT fk_cartitems_cart
-    FOREIGN KEY (`cart_id`)
-    REFERENCES `sql_store`.`cart` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION;
