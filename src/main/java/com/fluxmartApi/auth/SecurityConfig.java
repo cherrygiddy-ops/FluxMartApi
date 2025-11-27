1b388c48f456a87881742ac27fc517b05d30d016
Morris <morrischerryg@gmail.com>
2025-11-27 17:05:07 +0300

    Fix The CORBS Error

--- src/main/java/com/fluxmartApi/auth/SecurityConfig.java
+++ src/main/java/com/fluxmartApi/auth/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://fluxmart.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://fluxmart.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
